4927051ebdd594765039378dff6a33527d14d0ea
itwinwin996-svg <it.winwin996@gmail.com>
2026-06-10 13:31:25 +0700

    update yum

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+mkdir -p /root/repo-backup
+mv /etc/yum.repos.d/*.repo /root/repo-backup/ 2>/dev/null
+
+curl -o /etc/yum.repos.d/CentOS-Vault.repo \
+https://github.com/rooriginal20-wq/centos6.git
+
+yum clean all
+rm -rf /var/cache/yum/*
+yum makecache
+yum repolist
+yum -y install wget nano curl vim net-tools telnet bind-utils unzip zip
+rpm -Uvh https://repo.zabbix.com/zabbix/7.0/rhel/6/x86_64/zabbix-release-latest-7.0.el6.noarch.rpm
+yum clean all
+yum -y install zabbix-agent
+
