2bb0b8ae9740752f86f1a14214448a535f06f00c
itwinwin996-svg <it.winwin996@gmail.com>
2026-06-10 14:16:55 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,3 +14,11 @@ yum -y install wget nano curl vim net-tools telnet bind-utils unzip zip
 rpm -Uvh https://repo.zabbix.com/zabbix/7.0/rhel/6/x86_64/zabbix-release-latest-7.0.el6.noarch.rpm
 yum clean all
 yum -y install zabbix-agent
+
+CONFIG_FILE="/etc/zabbix/zabbix_agentd.conf"
+ZABBIX_SERVER_IP="192.168.8.142"
+
+sed -i "s/^Server=127.0.0.1/Server=$ZABBIX_SERVER_IP/g" "$CONFIG_FILE"
+sed -i "s/^ServerActive=127.0.0.1/ServerActive=$ZABBIX_SERVER_IP/g" "$CONFIG_FILE"
+chkconfig zabbix-agent on
+service zabbix-agent restart
